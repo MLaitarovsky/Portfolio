ed8c287006fcbd4b2d5adeb81345ef8c3886f6f9
Michael <laytarovsky.michael@gmail.com>
2024-12-26 22:25:34 -0500

    Intial setup

---
+++ michael-laitarovsky/src/main.jsx
@@ -0,0 +1,26 @@
+/**
+ * @copyright 2024 Michael Laitarovsky
+ * @license Apache-2.0
+ */
+
+/**
+ * Node modules
+ */
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+/**
+ * Components
+ */
+import App from "./App.jsx";
+
+/**
+ * Css link
+ */
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
